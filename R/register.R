f687b0ff09dde76548b5bfaa0ba28a83382ee4e0
Rainer Krug <Rainer@krugs.de>
2020-08-18 16:03:00 +0200

    Change LEEF.Data to LEEF

--- R/register.R
+++ R/register.R
@@ -1,14 +1,14 @@
-#' Register the processing of bemovi data in the LEEF.Data package
+#' Register the processing of bemovi data in the LEEF package
 #'
 #' @return invisibly \code{TRUE} when completed successful
 #' @export
 #'
 register <- function() {
-  if (is.null(system.file(package = "LEEF.Data"))) {
+  if (is.null(system.file(package = "LEEF"))) {
     stop("This function requres the package to be installed!")
   }
-  LEEF.Data::add_pre_processor( pre_processor_bemovi )
-  LEEF.Data::add_extractor( extractor_bemovi )
+  LEEF::add_pre_processor( pre_processor_bemovi )
+  LEEF::add_extractor( extractor_bemovi )
 
   invisible(TRUE)
 }
