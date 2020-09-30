a60e4c0f08012c9e4efeb5d2bb7d05e5783901dd
Rainer M. Krug <Rainer@krugs.de>
2020-09-30 15:22:32 +0200

    raw_data_ok(): add check for cxd file in video description file

--- R/raw_data_ok.R
+++ R/raw_data_ok.R
@@ -24,5 +24,20 @@ raw_data_ok <- function(input) {
   ok$bemovi_extract <- file.exists( file.path(input, "bemovi", "bemovi_extract.yml") )
   ok$video_description <- file.exists( file.path(input, "bemovi", "video.description.txt") )
 
+  if (ok$video_description) {
+    vd <- read.table(file.path(input, "bemovi", "video.description.txt"), header = TRUE)
+    vf <- list.files(file.path(input, "bemovi"), pattern = "\\.cxd")
+    invd <- gsub("\\.cxd", "", vf) %in% vd$file
+    names(invd) <- vf
+    ok$videofile_in_video_description <- invd
+  } else {
+    ok$videofile_in_video_description <- FALSE
+  }
+  # ok$video_file_names <- bemovi.LEEF::check_video_file_names(
+  #   to.data = input,
+  #   raw.video.folder = "./",
+  #   video.description.folder = "."
+  # )
+
   return(ok)
 }
